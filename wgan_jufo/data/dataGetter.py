e76bca61e9c10362219ecbfaa8a7349bf94b7752
Maximilian Herzig <maximilian-herzig@hotmail.com>
2020-02-10 12:18:02 +0100

    Einfach ein kurzer Kommentar zur reCol() Methode...

--- wgan_jufo/data/dataGetter.py
+++ wgan_jufo/data/dataGetter.py
@@ -1,10 +1,12 @@
 import xlrd as x
 import numpy as np
 
-# Einlesen der Excel-Datei, Rückgabe eines Ereignisses (einer Reihe) als Array
-
+# Variable für Datenquelle initialisieren
 DATA_FILE = "augeralle.xlsx"
 def reCol(c):
+    '''
+    Methode zur Erfassung und Rückgabe der Ereignisdaten aus einer Exceltabelle
+    '''
     wb = x.open_workbook(DATA_FILE)
     s = wb.sheet_by_index(0)
     col = np.asarray([s.cell(c, r).value for r in range(1, s.ncols])
