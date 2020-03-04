22d54973199cbf06967816080a55d2321457e631
Maximilian Herzig <maximilian-herzig@hotmail.com>
2020-03-05 00:38:47 +0100

    Generator angefangen, aber ich bin echt müde

--- wgan_torch/models/wganGenerator.py
+++ wgan_torch/models/wganGenerator.py
@@ -6,8 +6,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
+
 class wganGenerator(nn.Module):
     def __init__(self):
         super(wganGenerator, self).__init__()
-
-
+        self.inL = nn.Linear(2, 3)
+        self.hid1 = nn.Linear(3, 4)
+        self.hid2 = nn.Conv2d(4, 4)
+        self.hid3 = nn.Linear(4, 6)
+        self.outL = nn.Linear(6, 6)
