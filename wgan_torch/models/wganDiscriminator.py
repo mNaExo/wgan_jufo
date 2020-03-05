94725d87ff66869549f84f6d2a2cf209df87619e
Maximilian Herzig <maximilian-herzig@hotmail.com>
2020-03-05 23:21:59 +0100

    Generator und Diskriminator erster Prototyp, nur noch großes. Hoffentlich klappt alles einigermaßen

--- wgan_torch/models/wganDiscriminator.py
+++ wgan_torch/models/wganDiscriminator.py
@@ -5,3 +5,31 @@ import torch.autograd as autograd
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+
+
+class wganDiscriminator(nn.Module):
+    """
+        Generatives neuronales Netzwerk. Konische Architektur,
+        einmal Conv2D, kann man ja mal probieren...
+    """
+    def __init__(self):
+        super(wganDiscriminator, self).__init__()
+        self.MAIN_MODULE = nn.Sequential(
+            nn.Linear(in_features=6, out_features=4),
+            nn.ReLU(),
+
+            nn.Conv2d(in_channels=4, out_channels=4),
+            nn.ReLU(),
+
+            nn.Linear(in_features=4, out_features=3),
+            nn.ReLU(),
+
+            nn.Linear(in_features=3, out_features=2),
+            nn.ReLU()
+
+        )
+        self.OUTPUT = nn.Tanh()
+
+    def forward(self, x):
+        x = self.MAIN_MODULE(x)
+        return self.OUTPUT(x)
--- wgan_torch/models/wganGenerator.py
+++ wgan_torch/models/wganGenerator.py
@@ -8,10 +8,27 @@ import torch.optim as optim
 
 
 class wganGenerator(nn.Module):
+    '''
+        Generatives neuronales Netzwerk. Konische Architektur,
+        einmal Conv2D, kann man ja mal probieren...
+    '''
     def __init__(self):
         super(wganGenerator, self).__init__()
-        self.inL = nn.Linear(2, 3)
-        self.hid1 = nn.Linear(3, 4)
-        self.hid2 = nn.Conv2d(4, 4)
-        self.hid3 = nn.Linear(4, 6)
-        self.outL = nn.Linear(6, 6)
+        self.MAIN_MODULE = nn.Sequential(
+            nn.Linear(in_features=2, out_features=3),
+            nn.ReLU(),
+
+            nn.Linear(in_features=3, out_features=4),
+            nn.ReLU(),
+
+            nn.Conv2d(in_channels=4, out_channels=4),
+            nn.ReLU(),
+
+            nn.Linear(in_features=4, out_features=6),
+            nn.ReLU()
+        )
+        self.OUTPUT = nn.Tanh()
+
+    def forward(self, x):
+        x = self.MAIN_MODULE(x)
+        return self.OUTPUT(x)
