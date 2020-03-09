57aae6511e35492c3ab778f49bb3859fb57ef1f9
Maximilian Herzig <maximilian-herzig@hotmail.com>
2020-03-09 09:30:39 +0100

    SOooo ein scheiß

--- wgan_torch/models/wganGenerator.py
+++ wgan_torch/models/wganGenerator.py
@@ -1,10 +1,10 @@
-import sys, os
+# import sys, os
 
 import torch
 import torch.autograd as autograd
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
+# import torch.optim as optim
 
 
 class wganGenerator(nn.Module):
@@ -12,6 +12,7 @@ class wganGenerator(nn.Module):
         Generatives neuronales Netzwerk. Konische Architektur,
         einmal Conv2D, kann man ja mal probieren...
     '''
+
     def __init__(self):
         super(wganGenerator, self).__init__()
         self.l1 = nn.Linear(in_features=2, out_features=3)
@@ -20,13 +21,14 @@ class wganGenerator(nn.Module):
 
         self.l3 = nn.Conv2d(in_channels=4, out_channels=4, kernel_size=2)
 
-        self.l4 = nn.Linear(in_features=2*2, out_features=6)
+        self.l4 = nn.Linear(in_features=2 * 2, out_features=6)
 
         self.OUTPUT = nn.Tanh()
 
     def forward(self, x):
+        x = x.autograd.vari
         x = F.relu(self.l1(x))
         x = F.relu(self.l2(x))
-        x = F.max_pool2d(F.relu(self.l3(x), (2, 2)))
+        x = F.max_pool2d(F.relu(self.l3(x)), (2, 2))
         x = F.relu(self.l4(x))
         return self.OUTPUT(x)
